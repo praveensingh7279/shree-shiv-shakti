5824381f237098ba013e97991cc8acea2e4ae164
sintu1609 <singhpraveen7279@gmail.com>
2026-06-22 19:29:23 +0530

    ten

--- script.js
+++ script.js
@@ -106,3 +106,9 @@ window.onclick=function(event){
     }
 
 }
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
